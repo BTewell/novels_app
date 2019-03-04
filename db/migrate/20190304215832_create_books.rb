897751945b813ab8e09c3f155c916bfc2148d167
“Brett <bretttewell@gmail.com>
2019-03-04 16:00:41 -0600

    initial commit, add controller, db, and model

---
+++ db/migrate/20190304215832_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :genre
+      t.string :year
+      t.string :cover_art
+
+      t.timestamps
+    end
+  end
+end
